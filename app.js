ac2bd08356730fde5f86780c11059c6e0e7bccda
Benny Luo <bennyluo95@gmail.com>
2021-10-29 15:39:18 -0400

    included production build

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     logger.error('error connection to MongoDB:', error.message)
   })
 
-//app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(middleware.reqLogger)
 
 //What is this:
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
-// app.use(express.static('build'))
+app.use(express.static('build'))
 
 app.use('/api/users', usersRouter)
 app.use('/api/results', resultsRouter)

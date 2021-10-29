e43613a7c773e23bbedda26b1e314f3c74b4733b
Benny Luo <bennyluo95@gmail.com>
2021-10-29 15:54:06 -0400

    fix sign up constraint error

--- controllers/users.js
+++ controllers/users.js
@@ -15,7 +15,7 @@ usersRouter.post("/", async (req,res) => {
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-    if (body.password.length || body.username.length < 3){
+    if (body.password.length < 3 || body.username.length < 3){
         return res.status(400).json({
             error: "Invalid login/password"
         })
